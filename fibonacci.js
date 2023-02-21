e38152bf4db1429cf46ee32eed29581b20197e29
Terry Kayondo <109131914+tkterry256@users.noreply.github.com>
2023-02-21 23:48:34 +0300

    Update fibonacci to pass test for real number

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,5 @@
 export default function fibonacci(num) {
-  if (num < 0) {
+  if (Number.isNaN(num) || !Number.isInteger(num) || num < 0) {
     throw new Error('n must be a positive non-zero integer');
   }
   if (num < 2) {
